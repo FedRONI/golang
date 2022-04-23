1836c988f3a3dd7bcbf6e661d2ab9ee2454dffd6
Шистеров Федор Павлович <pl8757@pl1.edu>
2022-04-23 15:22:34 +0500

    po4inil

--- db.sql
+++ db.sql
@@ -1,7 +1,7 @@
-CREATE database notice OWNER postgres;
-create table notes (
+-- CREATE DATABASE notice OWNER postgres;
+CREATE TABLE notes(
     id SERIAL PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     title TEXT,
-    info TEXT
+    info TEXT 
 )
--- main.go
+++ main.go
@@ -1,62 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+	"notice/controllers"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-pg/pg/v10"
 )
-
-type Note struct {
-	tableName struct{} `pg:"notes"`
-	Id        int64    `json:"id" pg:"id,pk"`
-	CreatedAt string   `json:"created_at", pg:"cratead_at"`
-	Title     string   `json:"title", pg:"title"`
-	Info      string   `json:"info", pg:"info"`
-}
-
-func pgDataBase() (con *pg.DB) {
-	address := fmt.Sprintf("%s:%s", "localhost", "5432")
-	options := &pg.Options{
-		User:     "postgress",
-		Password: "",
-		Addr:     address,
-		Database: "notice",
-		PoolSize: 50,
-	}
-
-	con = pg.Connect(options)
-	if con == nil {
-		log.Fatalf("АШИБКА ПАДКЛЮЧЕНИЯ К 415 БАЗЕ. 415 БАЗА ОТВЕТЬТЕ")
-	}
-	return
-}
-
-func SelectNotes() []Note {
-	var notes []Note
-	db := pgDataBase()
-
-	err := db.Model(&notes).Select()
-
-	if err != nil {
-		panic(err)
-	}
-
-	db.Close()
-
-	return notes
-}
 
 func Api(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"api": "notice",
 	})
-}
-
-func GetNotes(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, SelectNotes())
 }
 
 func main() {
@@ -65,7 +18,14 @@ func main() {
 
 	rApi := r.Group("/api")
 	{
-		rApi.GET("/notes", GetNotes)
+		rApi.GET("/notes", controller.GetNotes)
+		rNote := rApi.Group("/note")
+		{
+			rNote.POST("/add", controller.AddNote)
+			rNote.GET("/:id", controller.GetNote)
+			rNote.PUT("/edit", controller.EditNote)
+			rNote.DELETE("/:id", controller.DelNote)
+		}
 	}
 
 	r.Run("0.0.0.0:9090")
